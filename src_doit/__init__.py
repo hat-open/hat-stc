e5db22c47443d61b2b9ed9b9b408e688692176fe
bozo.kopic <bozo@kopic.xyz>
2021-08-11 18:44:40 +0200

    doit update

--- src_doit/__init__.py
+++ src_doit/__init__.py
@@ -3,6 +3,12 @@ import subprocess
 import sys
 
 from hat.doit import common
+from hat.doit.py import (build_wheel,
+                         run_pytest,
+                         run_flake8)
+from hat.doit.docs import (SphinxOutputType,
+                           build_sphinx,
+                           build_pdoc)
 
 
 __all__ = ['task_clean_all',
@@ -30,7 +36,7 @@ def task_build():
     """Build"""
 
     def build():
-        common.wheel_build(
+        build_wheel(
             src_dir=src_py_dir,
             dst_dir=build_dir / 'py',
             src_paths=list(common.path_rglob(src_py_dir,
@@ -46,41 +52,20 @@ def task_build():
 
 def task_check():
     """Check with flake8"""
-    return {'actions': [(_run_flake8, [src_py_dir]),
-                        (_run_flake8, [pytest_dir])]}
+    return {'actions': [(run_flake8, [src_py_dir]),
+                        (run_flake8, [pytest_dir])]}
 
 
 def task_test():
     """Test"""
-
-    def run(args):
-        subprocess.run([sys.executable, '-m', 'pytest',
-                        '-s', '-p', 'no:cacheprovider',
-                        *(args or [])],
-                       cwd=str(pytest_dir),
-                       check=True)
-
-    return {'actions': [run],
+    return {'actions': [lambda args: run_pytest(pytest_dir, *(args or []))],
             'pos_arg': 'args'}
 
 
 def task_docs():
     """Docs"""
-
-    def build():
-        common.sphinx_build(common.SphinxOutputType.HTML, docs_dir,
-                            build_docs_dir)
-        subprocess.run([sys.executable, '-m', 'pdoc',
-                        '--html', '--skip-errors', '-f',
-                        '-o', str(build_docs_dir / 'py_api'),
-                        'hat.stc'],
-                       stdout=subprocess.DEVNULL,
-                       stderr=subprocess.DEVNULL,
-                       check=True)
-
-    return {'actions': [build]}
-
-
-def _run_flake8(path):
-    subprocess.run([sys.executable, '-m', 'flake8', str(path)],
-                   check=True)
+    return {'actions': [(build_sphinx, [SphinxOutputType.HTML,
+                                        docs_dir,
+                                        build_docs_dir]),
+                        (build_pdoc, ['hat.stc',
+                                      build_docs_dir / 'py_api'])]}
