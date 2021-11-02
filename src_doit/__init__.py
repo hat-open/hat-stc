821d710fbcbf9294b2aa31765371ce2cbad6c3c5
bozo.kopic <bozo@kopic.xyz>
2021-11-02 15:14:12 +0100

    doit update

--- src_doit/__init__.py
+++ src_doit/__init__.py
@@ -39,8 +39,6 @@ def task_build():
         build_wheel(
             src_dir=src_py_dir,
             dst_dir=build_dir / 'py',
-            src_paths=list(common.path_rglob(src_py_dir,
-                                             blacklist={'__pycache__'})),
             name='hat-stc',
             description='Hat statechart engine',
             url='https://github.com/hat-open/hat-stc',
